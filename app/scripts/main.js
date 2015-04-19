878fa44a4ba592de511b35f38492d43e8ee59fdd
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-04-19 03:27:45 -0400

    Content and layout for second page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors: ['home', 'about', 'contact', 'fourthPage', 'lastPage'],
+    menu: '#navbar',
+    sectionsColor: ['#AA49B2', '#5CFED9', '#7BAABE', 'whitesmoke', '#000']
+  });
 });
