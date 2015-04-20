bbe109942994306d539debca8223f3b17023f978
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-04-19 20:30:54 -0400

    Add content and styling for Contact and Work pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,9 @@
 
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['home', 'about', 'contact', 'fourthPage', 'lastPage'],
+    anchors: ['home', 'about', 'contact', 'work'],
     menu: '#navbar',
-    sectionsColor: ['#AA49B2', '#5CFED9', '#7BAABE', 'whitesmoke', '#000']
+    sectionsColor: ['#AA49B2', '#5CFED9', '#2DACBE', '#C8BBC9'],
+    scrollOverflow: true
   });
 });
