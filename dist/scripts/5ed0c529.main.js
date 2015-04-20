274bc9eda511a43cc884c79c47b4a0bdcbfec712
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-04-19 23:05:20 -0400

    New build

---
+++ dist/scripts/5ed0c529.main.js
@@ -0,0 +1,1 @@
+"use strict";$(document).ready(function(){$("#fullpage").fullpage({anchors:["home","about","contact","work"],menu:"#navbar",sectionsColor:["#AA49B2","#5CFED9","#2DACBE","#C8BBC9"],scrollOverflow:!0})});
